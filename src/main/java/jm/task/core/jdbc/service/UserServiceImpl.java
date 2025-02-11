710e41c6a09234db3c71ad320306dba697104b4d
RustamMamedov2625 <mamedovrustam2509@gmail.com>
2025-02-12 01:09:49 +0400

    1.1.3 Make UserServiceImpl and toString() for User

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,46 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private final UserDao USERDAO;
+
+    public UserServiceImpl() {
+        this.USERDAO = new UserDaoJDBCImpl();
+    }
+    @Override
     public void createUsersTable() {
-
+        USERDAO.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
-
+        USERDAO.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        USERDAO.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
+        USERDAO.removeUserById(id);
 
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    @Override
+    public List<User> getAllUsers()
+    {
+        return USERDAO.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
-
+        USERDAO.cleanUsersTable();
     }
 }

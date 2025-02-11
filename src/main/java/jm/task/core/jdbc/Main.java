4ea84352eeda09b7076c975e670a79376fde9609
RustamMamedov2625 <mamedovrustam2509@gmail.com>
2025-02-12 01:15:44 +0400

    1.1.3 Make Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,31 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Smith", (byte) 30);
+        userService.saveUser("Isaac", "Rith", (byte) 12);
+        userService.saveUser("Ruzhie", "Bondarchuka", (byte) 25);
+        userService.saveUser("Evgeniy", "Avangardov", (byte) 54);
+
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
     }
 }

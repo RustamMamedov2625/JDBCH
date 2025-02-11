06fd3b24c339c03d3e37081b29494136e8165f92
RustamMamedov2625 <mamedovrustam2509@gmail.com>
2025-02-11 20:56:06 +0400

    1.1.3 Add databse connection logic in Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//Класс для работы с подключением к базе данных
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres"; //Адрес бд
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "251005";
+
+    //Метод для установления подключения с бд. Возвращает либо объект Connection или null в случае ошибки.
+    public static Connection getConnection() {
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch(SQLException e){
+            e.printStackTrace(); //Выводим ошибку, если подключение не удалось.
+        }
+        return connection;
+    }
 }

51c8ef5a5a252389bee04079239683074e338a00
RustamMamedov2625 <mamedovrustam2509@gmail.com>
2025-02-13 16:51:41 +0400

    1.1.4 Util was created

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,39 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 //Класс для работы с подключением к базе данных
 public class Util {
+    private static SessionFactory sessionFactory = buildSessionFactory();
 
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.URL", "jdbc:postgresql://localhost:5432/postgres");
+            configuration.setProperty("hibernate.connection.username", "postgres");
+            configuration.setProperty("hibernate.connection.password", "251005");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+            configuration.addAnnotatedClass(User.class);
+
+            return configuration.buildSessionFactory();
+        }catch (Throwable e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown(){
+        sessionFactory.close();
+    }
 }

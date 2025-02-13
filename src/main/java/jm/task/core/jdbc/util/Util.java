e3b28c9ba4e12a242ef8319b85ae74af54d2d499
RustamMamedov2625 <mamedovrustam2509@gmail.com>
2025-02-13 23:40:03 +0400

    1.1.4 ..\resources\hibernate.cfg.xml was removed and Util was changed

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -11,7 +11,14 @@ public class Util {
     private static SessionFactory buildSessionFactory() {
         try {
             Configuration configuration = new Configuration();
-            configuration.configure(); // Загружает настройки из hibernate.cfg.xml
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+            configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres");
+            configuration.setProperty("hibernate.connection.username", "postgres");
+            configuration.setProperty("hibernate.connection.password", "251005");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+            configuration.setProperty("hibernate.show_sql", "true");
+            configuration.addAnnotatedClass(jm.task.core.jdbc.model.User.class);
             return configuration.buildSessionFactory();
         }catch (Throwable e) {
             throw new ExceptionInInitializerError(e);
